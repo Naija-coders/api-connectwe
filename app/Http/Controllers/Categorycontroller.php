f772b3eab0e0c82c18cd697dc5930a8355dba24d
Oghenemano <oghenemano@MacBook-Pro.local>
2022-05-04 13:09:50 +0300

    sorting the data in decending order

--- app/Http/Controllers/Categorycontroller.php
+++ app/Http/Controllers/Categorycontroller.php
@@ -17,7 +17,7 @@ class Categorycontroller extends Controller
         ->select('users.name', 'users.phone_number', 'users.email', 'Services.services_id', 'Services.about', 'Services.title','Services.updated_date' ,'Services.image', 'Services.categories_id','Services.location', 'Services.price', 'categories.type', 'categories.job_advertisement')
         ->join('categories', 'Services.categories_id', 'categories.categories_id' )->join(
             'users', 'Services.id', 'users.id'
-        )->get();
+        )->orderByDesc('Services.updated_date')->get();
         
         return $users;
     }
