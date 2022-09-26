3d1691e3b4444356a45cbec0e4ea75d60d0f13b5
Oghenemano <oghenemano@MacBook-Pro.local>
2022-09-26 13:19:05 +0300

    adding agency login

--- app/Http/Controllers/RegistrationController.php
+++ app/Http/Controllers/RegistrationController.php
@@ -2,13 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\agencies;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\DB;
 use App\Models\User;
 use App\Models\businesses;
-
-
+use App\Models\contractors;
 
 class RegistrationController extends Controller
 {
@@ -87,6 +87,68 @@ class RegistrationController extends Controller
             $token = $user->createToken('auth_token')->plainTextToken;
 
             $business = new businesses;
+
+            $business->id = $user->id;
+            $business->company_name = $user->name;
+            $business->company_size = $request->company_size;
+            $business->is_business = 1;
+            $business->location = $request->location;
+            $business->save();
+            $response = [
+                'user' => $user,
+                'auth_token' => $token,
+                'businesses' => $business,
+            ];
+            return response($response, 201);
+        }
+    }
+    function agencyRegistration(Request $request)
+    {
+        $user = new User();
+        $user->name = $request->company_name;
+        $user->email = $request->email;
+        $user->password = Hash::make($request->password);
+        $result = $user->save();
+        if (!$result) {
+
+            return response([
+                'Message' => ['These credential do not match our records']
+            ], 404);
+        } else {
+            $token = $user->createToken('auth_token')->plainTextToken;
+
+            $business = new agencies();
+
+            $business->id = $user->id;
+            $business->company_name = $user->name;
+            $business->company_size = $request->company_size;
+            $business->is_business = 1;
+            $business->location = $request->location;
+            $business->save();
+            $response = [
+                'user' => $user,
+                'auth_token' => $token,
+                'businesses' => $business,
+            ];
+            return response($response, 201);
+        }
+    }
+    function contractorRegistration(Request $request)
+    {
+        $user = new User();
+        $user->name = $request->company_name;
+        $user->email = $request->email;
+        $user->password = Hash::make($request->password);
+        $result = $user->save();
+        if (!$result) {
+
+            return response([
+                'Message' => ['These credential do not match our records']
+            ], 404);
+        } else {
+            $token = $user->createToken('auth_token')->plainTextToken;
+
+            $business = new contractors();
 
             $business->id = $user->id;
             $business->company_name = $user->name;
---
+++ app/Models/contractors.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class contractors extends Model
+{
+    use HasFactory;
+    public $timestamps = false;
+}
