1261db0ec633383386d532571d6f7d29c662ceec
Oghenemano <oghenemano@MacBook-Pro.local>
2022-09-30 00:31:10 +0300

    user

--- app/Http/Controllers/getuserprofileController.php
+++ app/Http/Controllers/getuserprofileController.php
@@ -10,11 +10,17 @@ class getuserprofileController extends Controller
 {
     //
     function getusersprofile(Request $req){
-        
+      $user_id = DB::table('auth_token')->where('id', $user_token)->first()
+if($user_id){
+  $user = Auth::user();
+  return response()->json($user,200)
+}
+else{
+  return response()->json(["message"=>"not authenticated"],404)
+}
        return (
         $user_id = DB::table('auth_token')->where('id', $user_token)->first()
-         echo $user_id->id;
-         echo $user_name = Auth::user()->name;
+        
        );
 
 
