83c65fb63a0e18e35ebdcfa1881db9f0c4cc2ee2
Oghenemano <oghenemano@MacBook-Pro.local>
2022-09-30 00:35:35 +0300

    auth

--- app/Http/Controllers/getuserprofileController.php
+++ app/Http/Controllers/getuserprofileController.php
@@ -4,26 +4,20 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 
 class getuserprofileController extends Controller
 {
-    //
-    function getusersprofile(Request $req){
-      $user_id = DB::table('auth_token')->where('id', $user_token)->first()
-if($user_id){
-  $user = Auth::user();
-  return response()->json($user,200)
-}
-else{
-  return response()->json(["message"=>"not authenticated"],404)
-}
-       return (
-        $user_id = DB::table('auth_token')->where('id', $user_token)->first()
-        
-       );
+  //
+  function getusersprofile(Request $req)
+  {
 
 
 
+    $user = Auth::user();
+    if ($user) {
+      return response()->json($user, 200);
     }
+  }
 }
