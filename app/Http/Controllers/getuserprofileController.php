21de26fc78d6ead8954cc8f18391424df0796b5c
Oghenemano <oghenemano@MacBook-Pro.local>
2022-09-29 23:24:32 +0300

    checking get userprofile

--- app/Http/Controllers/getuserprofileController.php
+++ app/Http/Controllers/getuserprofileController.php
@@ -8,16 +8,15 @@ use Illuminate\Support\Facades\Auth;
 
 class getuserprofileController extends Controller
 {
-    //
-    function getusersprofile(Request $req){
-        
-       return (
-        $user_id = DB::table('auth_token')->where('id', $user_token)->first();
-         echo $user_id->id;
-         echo $user_name = Auth::user()->name;
-       )
+  //
+  function getusersprofile(Request $req)
+  {
 
-
-
+    $user = $req->user();
+    if ($user) {
+      return response()->json($user, 200);
+    } else {
+      return response()->json(['message' => "user not found"], 404);
     }
+  }
 }
