a175ed8e8a1ce5f1745c2714ba144b04df02f8ad
Oghenemano <oghenemano@MacBook-Pro.local>
2022-10-12 10:36:56 +0300

    adding migration for table price, projects,tags,categories

---
+++ database/migrations/2022_10_12_072903_create_projects_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 225);
+            $table->foreignId('categories_id')->references('id')->on('categories')->onDelete('cascade');
+            $table->foreignId('tags_id')->references('id')->on('tags')->onDelete('cascade');
+            $table->string('overview');
+            $table->string('location_type');
+            $table->string('location');
+            $table->foreignId('price_id')->references('id')->on('prices')->onDelete('cascade');
+            $table->foreignId('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('projects');
+    }
+};
