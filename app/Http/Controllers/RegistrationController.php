2e6f2f45ff682ed36e550ec1ed2027cfa306ee65
Oghenemanoomogha <manoomogha@yahoo.com>
2022-10-17 16:52:21 +0300

    removing email verification

--- app/Http/Controllers/RegistrationController.php
+++ app/Http/Controllers/RegistrationController.php
@@ -108,7 +108,7 @@ class RegistrationController extends Controller
         $user->email = $request->email;
         $user->password = Hash::make($request->password);
         $result = $user->save();
-        $sendverification = $user->sendEmailVerificationNotification();
+
 
         if (!$result) {
 
@@ -117,7 +117,7 @@ class RegistrationController extends Controller
             ], 404);
         } else {
             $token = $user->createToken('auth_token')->plainTextToken;
-            $sendverification;
+
             $business = new agencies();
 
             $business->user_id = $user->id;
