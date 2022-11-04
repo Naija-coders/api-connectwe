ab686849cd5d9d5cdfe0cf94e5773a142a72b986
Oghenemanoomogha <manoomogha@yahoo.com>
2022-11-04 13:14:18 +0200

    where clause issue

--- app/Http/Controllers/ServiceController.php
+++ app/Http/Controllers/ServiceController.php
@@ -91,7 +91,7 @@ class ServiceController extends Controller
     }
     function ExplorePaginatedSubcategory(Request $request)
     {
-        $service = DB::table('services')->select('services.*', 'images.image_url', 'users.name', 'users.profile_photo_path', 'tags.tag_name', 'categories.type', 'service_prices.currency', 'service_prices.price')->join('categories', 'categories.id', 'services.categories_id')->join('images', 'images.id', 'services.images_id')->join('users', 'users.id', 'services.users_id')->join('tags', 'tags.id', 'services.tags_id')->join('service_prices', 'service_prices.id', 'services.price_id')->where('tags.name', $request->tags)->paginate(12);
+        $service = DB::table('services')->select('services.*', 'images.image_url', 'users.name', 'users.profile_photo_path', 'tags.tag_name', 'categories.type', 'service_prices.currency', 'service_prices.price')->join('categories', 'categories.id', 'services.categories_id')->join('images', 'images.id', 'services.images_id')->join('users', 'users.id', 'services.users_id')->join('tags', 'tags.id', 'services.tags_id')->join('service_prices', 'service_prices.id', 'services.price_id')->where('tags.tag_name', $request->tags)->paginate(12);
         return $service;
     }
     function Service(Request $request)
